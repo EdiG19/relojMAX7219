43bf9f2f1c55152b7eb225150759959c23c13651
Edi <guardiaedier2004@gmail.com>
2025-12-12 14:21:01 -0500

    Primer Commit

---
+++ CodigoModuloMAX7219/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <MatrizLed.h>
+
+/*
+ * Contador con varias pantallas
+ */
+
+MatrizLed pantalla;
+int contador = 0;
+
+void setup() {
+  pantalla.begin(12, 11, 10, 2); // dataPin, clkPin, csPin, numero de matrices de 8x8
+  pantalla.rotar(false);
+}
+
+void loop() { 
+  pantalla.borrar();
+  pantalla.escribirCifra(contador);
+  contador++;
+  delay(1000);
+}
